3d178082929d16c8a076839d4857ade66a955f88
Vanessa Sochat <vsochat@stanford.edu>
2019-03-22 18:16:45 -0400

    adding start of files
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

---
+++ README.md
@@ -0,0 +1,10 @@
+# TraceErr (Python)
+
+Functions for understanding and collecting errors.
+
+**under development**
+
+## Licenses
+
+This code is licensed under the MPL 2.0 [LICENSE](LICENSE).
+
---
+++ tracerr/version.py
@@ -0,0 +1,19 @@
+
+# Copyright (C) 2018-2019 Vanessa Sochat.
+
+# This Source Code Form is subject to the terms of the
+# Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+__version__ = "0.0.1"
+AUTHOR = 'Vanessa Sochat'
+AUTHOR_EMAIL = 'vsochat@stanford.edu'
+NAME = 'tracerr'
+PACKAGE_URL = "http://www.github.com/vsoch/error"
+KEYWORDS = 'errors, database, management'
+DESCRIPTION = "python functions for understanding errors"
+LICENSE = "LICENSE"
+
+INSTALL_REQUIRES = (
+)
